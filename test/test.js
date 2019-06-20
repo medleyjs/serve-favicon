b5218301466b29c18a1d59ddedc0de2cf90f00f1
Nathan Woltman <nwoltman@outlook.com>
2019-06-19 22:29:09 -0400

    refactor(test): Update devDependencies and refactor tests

--- test/test.js
+++ test/test.js
@@ -5,85 +5,93 @@ const fs = require('fs');
 const etag = require('etag');
 const medley = require('@medley/medley');
 const path = require('path');
+const selfRequest = require('@medley/self-request');
 const serveFavicon = require('..');
 
 const faviconPath = path.join(__dirname, 'favicon.ico');
 const faviconContent = fs.readFileSync(faviconPath);
 const faviconETag = etag(faviconContent);
 
+function makeApp() {
+  const app = medley();
+  app.register(selfRequest, {
+    gotDefaults: {encoding: null},
+  });
+  return app;
+}
+
 describe('serve-favicon', () => {
 
-  it('should accept the `favicon` option as a path string', () => {
-    const app = medley();
+  it('should accept the `favicon` option as a path string', async () => {
+    const app = makeApp();
 
     app.register(serveFavicon, {
       favicon: faviconPath,
     });
 
-    return app.inject('/favicon.ico').then((res) => {
-      assert.strictEqual(res.statusCode, 200);
-      assert.deepStrictEqual(res.rawPayload, faviconContent);
-      assert.strictEqual(res.headers.etag, faviconETag);
-      assert.strictEqual(res.headers['content-type'], 'image/x-icon');
-      assert.strictEqual(res.headers['cache-control'], 'public,max-age=31536000');
-    });
+    const res = await app.request('/favicon.ico');
+
+    assert.strictEqual(res.statusCode, 200);
+    assert.deepStrictEqual(res.body, faviconContent);
+    assert.strictEqual(res.headers.etag, faviconETag);
+    assert.strictEqual(res.headers['content-type'], 'image/x-icon');
+    assert.strictEqual(res.headers['cache-control'], 'public,max-age=31536000');
   });
 
-  it('should accept the `favicon` option as a buffer', () => {
-    const app = medley();
+  it('should accept the `favicon` option as a buffer', async () => {
+    const app = makeApp();
 
     app.register(serveFavicon, {
       favicon: faviconContent,
     });
 
-    return app.inject('/favicon.ico').then((res) => {
-      assert.strictEqual(res.statusCode, 200);
-      assert.deepStrictEqual(res.rawPayload, faviconContent);
-      assert.strictEqual(res.headers.etag, faviconETag);
-      assert.strictEqual(res.headers['content-type'], 'image/x-icon');
-      assert.strictEqual(res.headers['cache-control'], 'public,max-age=31536000');
-    });
+    const res = await app.request('/favicon.ico');
+
+    assert.strictEqual(res.statusCode, 200);
+    assert.deepStrictEqual(res.body, faviconContent);
+    assert.strictEqual(res.headers.etag, faviconETag);
+    assert.strictEqual(res.headers['content-type'], 'image/x-icon');
+    assert.strictEqual(res.headers['cache-control'], 'public,max-age=31536000');
   });
 
-  it('should accept the `maxAge` option', () => {
-    const app = medley();
+  it('should accept the `maxAge` option', async () => {
+    const app = makeApp();
 
     app.register(serveFavicon, {
       favicon: faviconContent,
       maxAge: 1,
     });
 
-    return app.inject('/favicon.ico').then((res) => {
-      assert.strictEqual(res.statusCode, 200);
-      assert.deepStrictEqual(res.rawPayload, faviconContent);
-      assert.strictEqual(res.headers.etag, faviconETag);
-      assert.strictEqual(res.headers['content-type'], 'image/x-icon');
-      assert.strictEqual(res.headers['cache-control'], 'public,max-age=1');
-    });
+    const res = await app.request('/favicon.ico');
+
+    assert.strictEqual(res.statusCode, 200);
+    assert.deepStrictEqual(res.body, faviconContent);
+    assert.strictEqual(res.headers.etag, faviconETag);
+    assert.strictEqual(res.headers['content-type'], 'image/x-icon');
+    assert.strictEqual(res.headers['cache-control'], 'public,max-age=1');
   });
 
 
   describe('if the If-None-Match header does not match the ETag', () => {
 
-    it('should send a 200 response', () => {
-      const app = medley();
+    it('should send a 200 response', async () => {
+      const app = makeApp();
 
       app.register(serveFavicon, {
         favicon: faviconContent,
       });
 
-      return app.inject({
+      const res = await app.request({
         url: '/favicon.ico',
         headers: {
           'If-None-Match': faviconETag + '_',
         },
-      }).then((res) => {
-        assert.strictEqual(res.statusCode, 200);
-        assert.deepStrictEqual(res.rawPayload, faviconContent);
-        assert.strictEqual(res.headers.etag, faviconETag);
-        assert.strictEqual(res.headers['content-type'], 'image/x-icon');
-        assert.strictEqual(res.headers['cache-control'], 'public,max-age=31536000');
       });
+      assert.strictEqual(res.statusCode, 200);
+      assert.deepStrictEqual(res.body, faviconContent);
+      assert.strictEqual(res.headers.etag, faviconETag);
+      assert.strictEqual(res.headers['content-type'], 'image/x-icon');
+      assert.strictEqual(res.headers['cache-control'], 'public,max-age=31536000');
     });
 
   });
@@ -91,69 +99,68 @@ describe('serve-favicon', () => {
 
   describe('if the If-None-Match header matches the ETag', () => {
 
-    it('should send a 304 response if there is no Cache-Control header', () => {
-      const app = medley();
+    it('should send a 304 response if there is no Cache-Control header', async () => {
+      const app = makeApp();
 
       app.register(serveFavicon, {
         favicon: faviconContent,
       });
 
-      return app.inject({
+      const res = await app.request({
         url: '/favicon.ico',
         headers: {
           'If-None-Match': faviconETag,
         },
-      }).then((res) => {
-        assert.strictEqual(res.statusCode, 304);
-        assert.strictEqual(res.payload, '');
-        assert.strictEqual(res.headers.etag, faviconETag);
-        assert.strictEqual(res.headers['content-type'], undefined);
-        assert.strictEqual(res.headers['cache-control'], 'public,max-age=31536000');
+        encoding: 'utf8',
       });
+      assert.strictEqual(res.statusCode, 304);
+      assert.strictEqual(res.body, '');
+      assert.strictEqual(res.headers.etag, faviconETag);
+      assert.strictEqual(res.headers['content-type'], undefined);
+      assert.strictEqual(res.headers['cache-control'], 'public,max-age=31536000');
     });
 
-    it('should send a 304 response if the Cache-Control header does not contain "no-cache"', () => {
-      const app = medley();
+    it('should send a 304 response if the Cache-Control header does not contain "no-cache"', async () => {
+      const app = makeApp();
 
       app.register(serveFavicon, {
         favicon: faviconContent,
       });
 
-      return app.inject({
+      const res = await app.request({
         url: '/favicon.ico',
         headers: {
           'Cache-Control': 'cache',
           'If-None-Match': faviconETag,
         },
-      }).then((res) => {
-        assert.strictEqual(res.statusCode, 304);
-        assert.strictEqual(res.payload, '');
-        assert.strictEqual(res.headers.etag, faviconETag);
-        assert.strictEqual(res.headers['content-type'], undefined);
-        assert.strictEqual(res.headers['cache-control'], 'public,max-age=31536000');
+        encoding: 'utf8',
       });
+      assert.strictEqual(res.statusCode, 304);
+      assert.strictEqual(res.body, '');
+      assert.strictEqual(res.headers.etag, faviconETag);
+      assert.strictEqual(res.headers['content-type'], undefined);
+      assert.strictEqual(res.headers['cache-control'], 'public,max-age=31536000');
     });
 
-    it('should send a 200 response if the Cache-Control header contains "no-cache"', () => {
-      const app = medley();
+    it('should send a 200 response if the Cache-Control header contains "no-cache"', async () => {
+      const app = makeApp();
 
       app.register(serveFavicon, {
         favicon: faviconContent,
       });
 
-      return app.inject({
+      const res = await app.request({
         url: '/favicon.ico',
         headers: {
           'Cache-Control': 'no-cache',
           'If-None-Match': faviconETag,
         },
-      }).then((res) => {
-        assert.strictEqual(res.statusCode, 200);
-        assert.deepStrictEqual(res.rawPayload, faviconContent);
-        assert.strictEqual(res.headers.etag, faviconETag);
-        assert.strictEqual(res.headers['content-type'], 'image/x-icon');
-        assert.strictEqual(res.headers['cache-control'], 'public,max-age=31536000');
       });
+      assert.strictEqual(res.statusCode, 200);
+      assert.deepStrictEqual(res.body, faviconContent);
+      assert.strictEqual(res.headers.etag, faviconETag);
+      assert.strictEqual(res.headers['content-type'], 'image/x-icon');
+      assert.strictEqual(res.headers['cache-control'], 'public,max-age=31536000');
     });
 
   });
